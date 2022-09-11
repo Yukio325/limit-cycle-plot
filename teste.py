f95af01e3bcafa400bd9f3f305b4ab661ee36f33
Eduardo Makita <makitayukio1@gmail.com>
2022-09-10 21:12:12 -0300

    add plotting changes

--- teste.py
+++ teste.py
@@ -27,16 +27,21 @@ class Ball():
         self.x += self.vx
         self.y += self.vy
 
+class Obstacle()
+    def __init__():
+        pass
+
 fig = plt.figure(figsize=(10, 7.8))
 
 vel = 0.05
 
-t = Point(1.5, .65) # target
-t = Point(1.2, 1.2)
+t = Point(1.3, .9) # target
+# t = Point(1.2, 1.2)
 q = Point(1.2, .65) # obstacle
-r_v = .075*(1+np.sqrt(2))/2 # avoidance radius
-#r = Point(1.0125, .5875, -45) # robot
-r = Point(.2, .2, 0)
+r_v = .085*(np.sqrt(2)) # avoidance radius
+r = Point(1.0125, .5875, 0) # robot
+r = Point(1, .3, 0)
+# r = Point(.2, .2, 0)
 
 # Plotting Field
 ax = plt.gca()
@@ -50,13 +55,13 @@ ax.add_patch(Circle((.75, .65), .2, fill=None, alpha=1))
 ax.add_patch(Arc((.15, .65), .1, 0.2, angle=0, theta1=-90, theta2=90, fill=None, alpha=1))
 ax.add_patch(Arc((1.5-.15, .65), .1, 0.2, angle=0, theta1=90, theta2=-90, fill=None, alpha=1))
 
-# ax.add_patch(Circle((1.3, .9), (.0427/2), fill=True, color="orange", alpha=1))
-#ax.add_patch(Rectangle((q.x-(.075/2), q.y-(.075/2)), .075, .075, fill=True, color="yellow", alpha=1))
-#ax.add_patch(Circle((q.x, q.y), r_v, fill=None, color="black", alpha=1))
+ax.add_patch(Circle((1.3, .9), (.0427/2), fill=True, color="orange", alpha=1))
+ax.add_patch(Rectangle((q.x-(.075/2), q.y-(.075/2)), .075, .075, fill=True, color="yellow", alpha=1))
+ax.add_patch(Circle((q.x, q.y), r_v, fill=None, color="black", alpha=1))
 tra = Affine2D().rotate_deg_around(r.x, r.y, r.theta) + ax.transData
 ax.add_patch(Rectangle((r.x-(.075/2), r.y-(.075/2)), .075, .075, fill=True, color="blue", alpha=1, transform=tra))
 
-x = np.linspace(r.x, 1.2, 10)
+x = np.linspace(r.x, t.x, 10)
 
 a = t.y - r.y
 b = r.x - t.x
@@ -69,10 +74,10 @@ d = (a*q.x + b*q.y + c)/np.sqrt(a**2 + b**2)
 path_x = []
 path_y = []
 
-for i in range(200):
+while round(r.x, 2) != t.x and round(r.y, 2) != t.y:
     dt = 0.01
     
-    x = np.linspace(r.x, 1.2, 10)
+    x = np.linspace(r.x, t.x, 10)
 
     a = t.y - r.y
     b = r.x - t.x
@@ -81,38 +86,40 @@ for i in range(200):
     y = (-a*x - c)/b
 
     k = 1 + (a**2/b**2)
-    l = -2*0.75 + (2*a*c)/b**2 + (2*a*0.65)/b
-    m = 0.75**2 + 0.65**2 - 0.2**2 + c**2/b**2 + (2*c*0.65)/b
+    l = -2*q.x + (2*a*c)/b**2 + (2*a*q.y)/b
+    m = q.x**2 + q.y**2 - r_v**2 + c**2/b**2 + (2*c*q.y)/b
     dscr = l**2 - 4*k*m
 
     if dscr <= 0:
-        color="green"
+        
+        path_x.append(r.x)
+        path_y.append(r.y)
+
+        r.x = r.x + dt
+        r.y = (-a*r.x - c)/b
     else:
-        color="white"
+        dx = r.x - q.x
+        dy = r.y - q.y
 
-    dx = r.x - 0.75
-    dy = r.y - 0.65
+        p = int(10*1/math.sqrt(dx**2 + dy**2))
 
-    p = round(5*1/math.sqrt(dx**2 + dy**2))
+        print(f"{p=}")
 
-    # print(f"{p=}")
-    re = 0.2
+        ddx = (d/abs(d))*dy + dx*(r_v**2 - dx**2 - dy**2)*p
+        ddy = -(d/abs(d))*dx + dy*(r_v**2 - dx**2 - dy**2)*p
 
-    ddx = (d/abs(d))*dy + dx*(re**2 - dx**2 - dy**2)*25
-    ddy = -(d/abs(d))*dx + dy*(re**2 - dx**2 - dy**2)*25
+        theta_d = math.atan2(ddy, ddx)
 
-    theta_d = math.atan2(ddy, ddx)
+        #print(theta_d)
 
-    #print(theta_d)
+        path_x.append(r.x)
+        path_y.append(r.y)
+        # plt.plot(x, y, color="grey", linewidth=1)
 
-    path_x.append(r.x)
-    path_y.append(r.y)
-    # plt.plot(x, y, color="grey", linewidth=1)
+        r.x = r.x + dt*ddx
+        r.y = r.y + dt*ddy
 
-    r.x = r.x + dt*ddx
-    r.y = r.y + dt*ddy
-
-    plt.plot(x, y, color=color, linewidth=1)
+    #plt.plot(x, y, color="blue", linewidth=1)
 
 plt.plot(path_x, path_y)
 
